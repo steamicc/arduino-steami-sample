77470c4437918a4dda7f7630789b7c9520f233fb
noroi28 <aline.dmnt83@gmail.com>
2026-04-17 15:19:53 +0200

    feat(Button): add example to turn on LEDs with buttons A, B and menu

---
+++ BUTTON/led_on.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+
+void setup_led_on()
+{
+    Serial.begin(115200);
+    delay(2000);
+
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+
+    pinMode(A_BUTTON, INPUT_PULLUP);
+    pinMode(B_BUTTON, INPUT_PULLUP);
+    pinMode(MENU_BUTTON, INPUT_PULLUP);
+}
+
+void loop_led_on()
+{
+    if (digitalRead(A_BUTTON) == LOW)
+    {
+        Serial.print("A_BUTTON pressed\n");
+        digitalWrite(LED_RED, HIGH);
+        delay(200);
+        digitalWrite(LED_RED, LOW);
+    }
+    else if (digitalRead(B_BUTTON) == LOW)
+    {
+        Serial.print("B_BUTTON pressed\n");
+        digitalWrite(LED_GREEN, HIGH);
+        delay(200);
+        digitalWrite(LED_GREEN, LOW);
+    }
+    else if (digitalRead(MENU_BUTTON) == LOW)
+    {
+        Serial.print("MENU_BUTTON pressed\n");
+        digitalWrite(LED_BLUE, HIGH);
+        delay(200);
+        digitalWrite(LED_BLUE, LOW);
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include "../LED/blink.cpp"
+#include "../BUTTON/led_on.cpp"
 
 void setup() {
-  setup_blink();
+//  setup_blink();
+  setup_led_on();
 }
 
 void loop() {
-  loop_blink();
+  // loop_blink();
+  loop_led_on();
 }
