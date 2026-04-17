2b8b927c85d3e24381c248ea7c11d9b9eeffc768
noroi28 <aline.dmnt83@gmail.com>
2026-04-17 15:49:02 +0200

    fix: Correction in main + addition of led_on in the readme.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
-#include "../LED/blink.cpp"
+// #include "../LED/blink.cpp"
 #include "../BUTTON/led_on.cpp"
 
-void setup() {
-//  setup_blink();
-  setup_led_on();
+void setup()
+{
+  // setup_blink(); /*../LED/blink.cpp*/
+  setup_led_on(); /*../BUTTON/led_on.cpp*/
 }
 
-void loop() {
-  // loop_blink();
-  loop_led_on();
+void loop()
+{
+  // loop_blink(); /*../LED/blink.cpp*/
+  loop_led_on(); /*../BUTTON/led_on.cpp*/
 }
