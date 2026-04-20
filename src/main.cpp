64a699b63e06d3e4e36eb3c0afdd2b961319fd8c
dumontALINE <aline.dmnt83@gmail.com>
2026-04-20 15:01:47 +0200

    feat(speaker): Add buzzer demo with happy, sad and epic sounds on button press.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
-#include "../LED/blink.cpp"
+//#include "../LED/blink.cpp"
+#include "../DEMO/buzzer.cpp"
 
 void setup() {
-  setup_blink();
+  //setup_blink();
+  setup_buzzer();
 }
 
 void loop() {
-  loop_blink();
+  //loop_blink();
+  loop_buzzer();
 }
